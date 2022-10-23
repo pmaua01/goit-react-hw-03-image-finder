a74b4b43b57d346406eb75e95ae56dbb7223aaf5
Mikhaylo Pobochikh <pma.ua01@gmail.com>
2022-10-23 22:01:36 +0300

    and but not all

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
 import { Spinner } from './Spinner/Spinner';
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-const API_KEY = '29770442-603a95234869127783f60906d';
+import { api } from './helpers/Api';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// const API_KEY = '29770442-603a95234869127783f60906d';
 
 export class App extends Component {
   state = {
@@ -25,63 +26,37 @@ export class App extends Component {
     try {
       // console.log('prevState', prevState.formSearchQuery);
       // console.log('this.state', this.state.formSearchQuery);
+
       if (
         prevState.formSearchQuery !== this.state.formSearchQuery &&
         this.state.formSearchQuery !== ''
       ) {
-        this.setState({ load: true });
-        const response = await axios
-          .get('https://pixabay.com/api/', {
-            params: {
-              key: API_KEY,
-              q: this.state.formSearchQuery,
-              image_type: 'photo',
-              orientation: 'horizontal',
-              per_page: 12,
-              page: this.state.page,
-            },
-          })
-          .finally(() => {
-            // this.setState({ load: false });
-            console.log('after all in try');
-          });
+        // await this.setState(() => {
+        //   return { page: 1 };
+        // });
+        await this.resetPage();
+
+        const response = await api(this.state.formSearchQuery, this.state.page);
 
         this.setState({
           load: false,
-          page: 1,
-          answerApi: response.data.hits,
-          answerLength: response.data.hits.length,
-          total: response.data.total,
+
+          answerApi: response.hits,
+          answerLength: response.hits.length,
+          total: response.total,
         });
       }
 
-      if (prevState.page !== this.state.page) {
-        this.setState({ load: true });
-        const response = await axios
-          .get('https://pixabay.com/api/', {
-            params: {
-              key: API_KEY,
-              q: this.state.formSearchQuery,
-              image_type: 'photo',
-              orientation: 'horizontal',
-              per_page: 12,
-              page: this.state.page,
-            },
-          })
-          .finally(() => {
-            // this.setState({ load: false });
-            console.log('after all in try');
-          });
-
-        this.setState(prevState => ({
-          answerApi: [...prevState.answerApi.concat(response.data.hits)],
-          answerLength: response.data.hits.length,
-          load: false,
-        }));
-      }
-
-      // this.setState({ load: false });
-      console.log('after all in try');
+      // if (prevState.page !== this.state.page) {
+      //   this.setState({ load: true });
+      //   const response = await api(this.state.formSearchQuery, this.state.page);
+      //   console.log('State after', response.hits);
+      //   this.setState(prevState => ({
+      //     answerApi: [...prevState.answerApi.concat(response.hits)],
+      //     answerLength: response.hits.length,
+      //     load: false,
+      //   }));
+      // }
     } catch (error) {
       if (prevState.formSearchQuery !== this.state.formSearchQuery) {
         console.log(error.message);
@@ -90,8 +65,33 @@ export class App extends Component {
     }
   }
 
+  onClickLoadMore = async () => {
+    this.setState({ load: true });
+    // await this.setState(prevState => ({
+    //   page: prevState.page + 1,
+    // }));
+    await this.incrementPage();
+
+    const response = await api(this.state.formSearchQuery, this.state.page);
+
+    this.setState(prevState => ({
+      answerApi: [...prevState.answerApi.concat(response.hits)],
+      answerLength: response.hits.length,
+      load: false,
+    }));
+  };
+
+  incrementPage = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  resetPage = () => {
+    this.setState({ page: 1 }, () => {});
+  };
+
   toggleModal = () => {
-    console.log('toggle');
     this.setState(({ openModal }) => ({
       openModal: !openModal,
     }));
@@ -108,13 +108,10 @@ export class App extends Component {
   };
 
   loadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+    this.onClickLoadMore();
   };
 
   onItemClick = url => {
-    // console.log(url);
     this.toggleModal();
     this.setState({ bigImgUrl: url });
   };
@@ -124,37 +121,41 @@ export class App extends Component {
   };
 
   render() {
-    const answerApiLength = this.state.answerApi.length;
-    console.log('In rendertotal', this.state.total);
-    console.log('In render answerLength', this.state.answerLength);
+    // const answerApiLength = this.state.answerApi.length;
+    const {
+      total,
+      load,
+      answerLength,
+      openModal,
+      answerApi,
+      bigImgUrl,
+      error,
+    } = this.state;
 
     return (
       <div>
         <Searchbar onChangeSearchQuery={this.onChangeSearchQuery}></Searchbar>
-        {this.state.total === 0 && <div>sorry no results found</div>}
+        {total === 0 && <div>sorry no results found</div>}
 
-        {answerApiLength !== 0 && (
+        {answerApi.length !== 0 && (
           <ImageGallery>
-            {this.state.load && <Spinner></Spinner>}
+            {load && <Spinner></Spinner>}
 
             <ImageGalleryItem
-              answerFromApi={this.state.answerApi}
+              answerFromApi={answerApi}
               onItemClick={this.onItemClick}
             ></ImageGalleryItem>
           </ImageGallery>
         )}
 
-        {this.state.answerLength !== 0 && this.state.answerLength >= 12 && (
-          <Button onClick={this.loadMore}></Button>
+        {answerLength !== 0 && answerLength >= 12 && (
+          <Button onClick={this.onClickLoadMore}></Button>
         )}
-        {this.state.openModal && (
-          <Modal
-            bigImg={this.state.bigImgUrl}
-            onClose={this.onCloseModal}
-          ></Modal>
+        {openModal && (
+          <Modal bigImg={bigImgUrl} onClose={this.onCloseModal}></Modal>
         )}
 
-        {this.state.error && <div>Ups</div>}
+        {error && <div>Oops something went wrong</div>}
       </div>
     );
   }
--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -2,6 +2,5 @@ import { Circles } from 'react-loader-spinner';
 import css from '../Spinner/Spinner.module.css';
 
 export const Spinner = () => {
-  console.log(css.Spinner);
   return <Circles wrapperClass={css.Spinner}></Circles>;
 };
